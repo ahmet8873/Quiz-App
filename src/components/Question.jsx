1386a1abae241f3dd9c922fd12c6f3d52c862e6f
Ahmet Avci <ahmetavci.8873@gmail.com>
2024-08-06 15:31:52 +0200

    correct-incorrect answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,25 +3,44 @@ import React, { useEffect, useState } from "react";
 const Question = ({ question, handleAnswer }) => {
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
 
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
   useEffect(() => {
     const { correct_answer, incorrect_answers } = question;
     const allAnswers = [correct_answer, ...incorrect_answers];
     setShuffledAnswers(allAnswers.sort(() => Math.random() - 0.5));
+
+    setSelectedAnswer(null);
   }, [question]);
 
   const { correct_answer } = question;
   const handleSelection = (selectedAnswer) => {
+    setSelectedAnswer(selectedAnswer);
     handleAnswer(selectedAnswer === correct_answer);
   };
 
   return (
     <div>
       <h3>{question.question}</h3>
-      {shuffledAnswers.map((answer, index) => (
-        <button key={index} onClick={() => handleSelection(answer)}>
-          {answer}
-        </button>
-      ))}
+      {shuffledAnswers.map((answer, index) => {
+        let buttonClass = "";
+        if (selectedAnswer) {
+          if (answer === correct_answer) {
+            buttonClass = "correct";
+          } else if (answer === selectedAnswer) {
+            buttonClass = "incorrect";
+          }
+        }
+        return (
+          <button
+            key={index}
+            onClick={() => handleSelection(answer)}
+            className={buttonClass}
+          >
+            {answer}
+          </button>
+        );
+      })}
     </div>
   );
 };
